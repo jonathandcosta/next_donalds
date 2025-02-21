06a99027747e753927dbc0024a3a30186f7a77ef
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-02-21 20:15:33 -0300

    chore: format recomended (good practices)

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { db } from "@/lib/prisma"
+import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
 
 interface RestaurantPageProps {
   params: Promise<{ slug: string }>
@@ -6,7 +6,7 @@ interface RestaurantPageProps {
 
 const RestaurantPage = async ({ params }: RestaurantPageProps) => {
   const { slug } = await params;
-  const restaurant = await db.restaurant.findFirst({ where: { slug } })
+  const restaurant = await getRestaurantBySlug(slug)
   return <h1>{restaurant?.name}</h1>
 }
 export default RestaurantPage
