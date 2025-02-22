114f5bef961d2fe4bc8e75075edc6002a063dd90
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-02-22 10:38:44 -0300

    feat: add hearder page dine-in

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { db } from "@/lib/prisma";
+import { ChevronLeft, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 interface RestaurantMenuPageProps {
@@ -14,15 +17,45 @@ const RestaurantMenuPage = async ({ params, searchParams }: RestaurantMenuPagePr
   const { slug } = await params
   const { consumptionMethod } = await searchParams
   if (!isConsumptionMethodValid(consumptionMethod)) {
-    return notFound
+    return notFound()
   }
 
 
   const restaurant = await db.restaurant.findUnique({ where: { slug } })
+  if (!restaurant) {
+    notFound()
+  }
 
-  return (<h1>MENU {slug} {consumptionMethod}</h1>);
+  return (
+    <div>
+      <div className="relative h-[250px] w-full">
+
+        <Button
+          variant="secondary"
+          size='icon'
+          className="absolute left-4 top-4 z-50 rounded-full"
+        >
+          <ChevronLeft />
+        </Button>
+
+        <Image
+          src={restaurant?.coverImageUrl}
+          alt={restaurant.name}
+          fill
+          className="object-cover"
+        />
+
+        <Button
+          variant="secondary"
+          size='icon'
+          className="absolute right-4 top-4 z-50 rounded-full"
+        >
+          <ScrollTextIcon />
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default RestaurantMenuPage;
 
-// http://localhost:3000/fsw-donalds/menu?consumptionMethod=dine-in
