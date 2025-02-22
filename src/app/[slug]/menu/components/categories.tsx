9e88ae082b3d176eaef8d74d25720a7bb9bb5a14
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-02-22 15:47:04 -0300

    feat: add menu category

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Prisma } from "@prisma/client";
 import { ClockIcon } from "lucide-react";
 import Image from "next/image";
@@ -16,23 +18,38 @@ interface RestaurantCategoriesProps {
 const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
 
   return (
-    <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl border bg-white p-5">
-      <div className="flex items-center gap-3">
-        <Image
-          src={restaurant.avatarImageUrl}
-          alt={restaurant.name}
-          height={45}
-          width={45}
-        />
-        <div>
-          <h2 className="text-lg font-semibold">{restaurant.name}</h2>
-          <p className="text-xs opacity-55">{restaurant.description}</p>
+    <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl border bg-white ">
+      <div className="p-5">
+        <div className="flex items-center gap-3">
+          <Image
+            src={restaurant.avatarImageUrl}
+            alt={restaurant.name}
+            height={45}
+            width={45}
+          />
+          <div>
+            <h2 className="text-lg font-semibold">{restaurant.name}</h2>
+            <p className="text-xs opacity-55">{restaurant.description}</p>
+          </div>
+        </div>
+        <div className="mt-3 flex items-center gap-1 text-xs text-green-500">
+          <ClockIcon size={12} />
+          <p>Aberto</p>
         </div>
       </div>
-      <div className="mt-3 flex items-center gap-1 text-xs text-green-500">
-        <ClockIcon size={12} />
-        <p>Aberto</p>
-      </div>
+      <ScrollArea className="w-full">
+        <div className="flex w-max space-x-4 p-4 pt-0">
+          {restaurant.menuCategories.map(category => (
+            <Button
+              key={category.id}
+              variant="secondary"
+              size='sm'
+              className="rounded-full">
+              {category.name}
+            </Button>
+          ))}
+        </div>
+      </ScrollArea>
     </div>);
 }
 
